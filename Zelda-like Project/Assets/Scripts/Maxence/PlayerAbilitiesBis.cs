2e56e0987927ebb54112b653d45131126a9dbff6
Maxence Drz <45963728+Mouloutouf@users.noreply.github.com>
2019-03-04 12:20:47 +0100

    Abilities Script is a massive boost in our business plan
    Cooldowns abilities fonctionnels + joystick instantiate nickel + quelques prefabs modifiés + bug animator corrigé

--- Zelda-like Project/Assets/Scripts/Maxence/PlayerAbilitiesBis.cs
+++ Zelda-like Project/Assets/Scripts/Maxence/PlayerAbilitiesBis.cs
@@ -17,7 +17,9 @@ public class PlayerAbilitiesBis : MonoBehaviour
 
     public bool stanceOne = false;
 
-    [SerializeField] private bool isReloading = false;
+    private bool[] cooldownIsOver = new bool[6];
+    private float[] coolDownTime = new float[6];
+    [SerializeField] private float[] startCoolDownTime;
 
     private string buttonName;
     private bool inputPressed;
@@ -27,13 +29,31 @@ public class PlayerAbilitiesBis : MonoBehaviour
 
     private PlayerControllerEzEz playerController;
 
+    private Vector2 transformPos;
+    private Vector2 aimPos;
+    [SerializeField] private float aimDistance;
+
     void Start()
     {
         playerController = GetComponent<PlayerControllerEzEz>();
+
+        for(int y = 0; y < 6; y++)
+        {
+            cooldownIsOver[y] = true;
+        }
+
+        for(int x = 0; x < 6; x++) //pas utile
+        {
+            coolDownTime[x] = startCoolDownTime[x];
+        }
     }
 
     void Update()
     {
+        transformPos = transform.position;
+
+        CoolDown();
+
         InputManager();
     }
 
@@ -41,68 +61,104 @@ public class PlayerAbilitiesBis : MonoBehaviour
     {
         if (stanceOne)
         {
-            if(Input.GetButtonDown("Potion1") && !isReloading)
+            if(Input.GetButtonDown("Potion1"))
             {
-                index = 0;
-                inputPressed = true;
-                buttonName = "Potion1";
+                if (cooldownIsOver[0])
+                {
+                    index = 0;
+                    inputPressed = true;
+                    buttonName = "Potion1";
+                }
             }
 
-            if (Input.GetButtonDown("Potion2") && !isReloading)
+            if (Input.GetButtonDown("Potion2"))
             {
-                index = 1;
-                inputPressed = true;
-                buttonName = "Potion2";
+                if (cooldownIsOver[1])
+                {
+                    index = 1;
+                    inputPressed = true;
+                    buttonName = "Potion2";
+                }                
             }
 
-            if (Input.GetButtonDown("Potion3") && !isReloading)
+            if (Input.GetButtonDown("Potion3"))
             {
-                index = 2;
-                inputPressed = true;
-                buttonName = "Potion3";
+                if (cooldownIsOver[2])
+                {
+                    index = 2;
+                    inputPressed = true;
+                    buttonName = "Potion3";
+                }                
             }
 
             if (inputPressed)
             {
-                Ability(index, true);
+                Ability(index, true, aimDistance);
             }
         }
 
         else if (!stanceOne)
         {
-            if (Input.GetButtonDown("Spell1") && !isReloading)
+            if (Input.GetButtonDown("Spell1"))
             {
-                index = 0;
-                inputPressed = true;
-                buttonName = "Spell1";
+                if (cooldownIsOver[3])
+                {
+                    index = 0;
+                    inputPressed = true;
+                    buttonName = "Spell1";
+                }                
             }
 
-            if (Input.GetButtonDown("Spell2") && !isReloading)
+            if (Input.GetButtonDown("Spell2"))
             {
-                index = 1;
-                inputPressed = true;
-                buttonName = "Spell2";
+                if (cooldownIsOver[4])
+                {
+                    index = 1;
+                    inputPressed = true;
+                    buttonName = "Spell2";
+                }                
             }
 
-            if (Input.GetButtonDown("Spell3") && !isReloading)
+            if (Input.GetButtonDown("Spell3"))
             {
-                index = 2;
-                inputPressed = true;
-                buttonName = "Spell3";
+                if (cooldownIsOver[5])
+                {
+                    index = 2;
+                    inputPressed = true;
+                    buttonName = "Spell3";
+                }                
             }
 
             if (inputPressed)
             {
-                Ability(index, false);
+                Ability(index, false, aimDistance);
             }
         }
     }
 
-    void Ability(int index, bool stance)
+    void CoolDown()
     {
-        Blueprint(index, stance);
+        for(int i = 0; i < 6; i++)
+        {
+            if (coolDownTime[i] <= 0.1f)
+            {
+                cooldownIsOver[i] = true;
+            }
 
-        if (Input.GetButtonUp(buttonName) && !isReloading)
+            else
+            {
+                coolDownTime[i] -= Time.deltaTime;
+            }
+        }        
+    }
+
+    void Ability(int index, bool stance, float aBDistance)
+    {
+        AimDirection(aBDistance);
+
+        Blueprint(index, stance, aBDistance);
+
+        if (Input.GetButtonUp(buttonName))
         {
             Release(index, stance);
         }
@@ -120,16 +176,22 @@ public class PlayerAbilitiesBis : MonoBehaviour
 
         if (rStance == false)
         {
-            Instantiate(spells[rIndex], transform.position, Quaternion.identity);
+            cooldownIsOver[rIndex + 3] = false;
+            coolDownTime[rIndex + 3] = startCoolDownTime[rIndex + 3];
+
+            Instantiate(spells[rIndex], aimPos, Quaternion.identity);
         }
 
         else if (rStance == true)
         {
-            Instantiate(potions[rIndex], transform.position, Quaternion.identity);
+            cooldownIsOver[rIndex] = false;
+            coolDownTime[rIndex] = startCoolDownTime[rIndex];
+
+            Instantiate(potions[rIndex], aimPos, Quaternion.identity);
         }
     }
 
-    void Blueprint(int bPIndex, bool bPStance)
+    void Blueprint(int bPIndex, bool bPStance, float bPDistance)
     {
         if (timer > time)
         {
@@ -150,12 +212,30 @@ public class PlayerAbilitiesBis : MonoBehaviour
                 }
             }
 
+            else
+            {                
+                theBluePrint.transform.position = aimPos;
+            }
+
             //là il faut immobiliser le joueur, et lui permettre de diriger le blueprint avec le joystick gauche
         }
 
         else
         {
             timer += Time.deltaTime;
+        }
+    }
+
+    void AimDirection(float distance)
+    {
+        if (playerController.lastX != 0 || playerController.lastY != 0)
+        {
+            float posX = playerController.lastX;
+            float posY = playerController.lastY;
+
+            Vector2 rawAimCoordinates = new Vector2(posX, posY);
+            Vector2 rawAimPos = rawAimCoordinates * distance;
+            aimPos = transformPos + rawAimPos;
         }
     }
 }
