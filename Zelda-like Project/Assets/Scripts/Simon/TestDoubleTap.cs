75aa6604d20e51487c4a7dcf683a2c2157a5d621
Simon <s.goldgrab@rubika-edu.com>
2019-03-05 14:28:44 +0100

    commit save

---
+++ Zelda-like Project/Assets/Scripts/Simon/TestDoubleTap.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDoubleTap : MonoBehaviour
+{
+
+    private bool ntm = false;
+
+    private float timer = 0.2f;
+
+    private bool oui = false;
+
+    private void Update()
+    {
+        
+        if(ntm)
+        {
+
+            timer -= Time.deltaTime;
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && !ntm)
+        {
+
+            ntm = true;       
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && ntm && timer > 0)
+        {
+
+            Debug.Log("DRINK");
+
+            oui = true;
+
+            ntm = false;
+
+            timer = 0.2f;
+
+        }
+
+        else if(timer <= 0 && !oui && ntm)
+        {
+
+            Debug.Log("BLUEPRINT");
+
+            ntm = false;
+
+            timer = 0.2f;
+
+        }
+
+    }
+
+}
---
+++ Zelda-like Project/Assets/Scripts/Simon/TestFilter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class TestFilter : MonoBehaviour
+{
+
+    private bool theStance = false;
+
+    public GameObject theCamera;
+    private PostProcessVolume theVolume;
+    private Vignette theVignette
+
+    private void Start()
+    {
+
+        theVolume = theCamera.GetComponent<PostProcessVolume>();
+
+        theVignette = theCamera.GetComponent<Vignette>();
+        
+    }
+
+    private void Update()
+    {
+
+        StartCoroutine(Blink());
+
+    }
+
+    IEnumerator Blink()
+    {
+
+        if(theStance)
+        {
+
+            theVolume.weight = 0;
+            theVignette.intensity = 0;
+            yield return new WaitForSeconds(1);
+
+        }
+
+        if(!theStance)
+        {
+
+            theVolume.weight = 1;
+            theVignette.intensity = 1;
+            yield return new WaitForSeconds(1);
+
+        }
+
+    }
+
+}
