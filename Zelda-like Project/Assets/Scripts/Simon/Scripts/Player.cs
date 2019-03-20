ace80e0958f949aaa5405a196999212a0b172c12
simon <s.goldgrab@rubika-edu.com>
2019-03-20 23:22:47 +0100

    Base architecture
    c le débu du comanceman

---
+++ Zelda-like Project/Assets/Scripts/Simon/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+
+    private void Start()
+    {
+
+        maxHealth = 5;
+        health = maxHealth;
+        damage = 1;
+        speed = 3.2f;
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+
+    }
+
+}
---
+++ Zelda-like Project/Assets/Scripts/Simon/Scripts/Templier.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Templier : Enemy
+{
+
+    private void Start()
+    {
+
+        maxHealth = 4;
+        health = maxHealth;
+        damage = 1;
+        speed = 2f;
+
+    }
+
+    public override void IdleBehavior()
+    {
+
+        base.IdleBehavior();
+
+    }
+
+    public override void AttackBehavior()
+    {
+
+        base.AttackBehavior();
+
+    }
+
+}
