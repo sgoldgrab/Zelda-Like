610a2776f911f2b598bd17b9a8b6fcaec9cfffa3
maelRicou <m.ricou@rubika-edu.com>
2019-04-26 15:37:09 +0100

    UI // Texte display on triggerEnter
    le joueur, à proximité des squelettes, crée un button qui déclenche une fonction qui affichera le texte

---
+++ Zelda-like Project/Assets/Scripts/Mael/Scripts/TextTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+    public GameObject textePourObjet;
+    public GameObject buttonTexte;
+
+    //public bool isTrigger;
+
+    private void Start()
+    {
+        textePourObjet.SetActive(false);
+        buttonTexte.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        Debug.Log("Touché");
+
+        if (player.gameObject.tag == "Player")
+        {
+            Debug.Log("Double Touché");
+            buttonTexte.SetActive(true);
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        textePourObjet.SetActive(false);
+        Debug.Log("Jesus meurt");
+    }
+
+    public void DisplayText()
+    {
+        textePourObjet.SetActive(true);
+        StartCoroutine("WaitForSec");
+        Debug.Log("Naissance de la bible");
+    }
+
+    /*private void Update()
+    {
+        if (isTrigger == true)
+        {
+            textePourObjet.SetActive(true);
+            StartCoroutine("WaitForSec");
+            Debug.Log("Naissance de la bible biblique");
+        }
+
+    }*/
+}
