d4e237bd8deaa71b95afa8049498cb5894f16f75
(rtbqe"t <s.goldgrab@rubika-edu.com>
2019-04-26 14:39:48 +0200

    début consos

---
+++ Zelda-like Project/Assets/Scripts/Simon/WorkOnSandBox/CharmOfRenewal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharmOfRenewal : PickUp
+{
+    //Heals the player every 2 second during 6 seconds, but locks him in his current stance during the effect
+
+    private PlayerState healthScript;
+    private PlayerStance stanceScript;
+
+    public override void Consume()
+    {
+        base.Consume();
+        healthScript = player.GetComponent<PlayerState>();
+        stanceScript = player.GetComponent<PlayerStance>();
+        StartCoroutine(CharmEffect(2f, 1));
+    }
+
+    IEnumerator CharmEffect(float time, int healingAmount)
+    {
+        //stanceScript.canSwitch = false;
+        yield return new WaitForSeconds(time);
+        //healthScript.TakeHeal(healingAmount);
+        yield return new WaitForSeconds(time);
+        //healthScript.TakeHeal(healingAmount);
+        yield return new WaitForSeconds(time);
+        //healthScript.TakeHeal(healingAmount);
+        //stanceScript.canSwitch = true;
+        Destroy(this);
+    }
+
+}
---
+++ Zelda-like Project/Assets/Scripts/Simon/WorkOnSandBox/LilyOfPetrification.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LilyOfPetrificaion : PickUp
+{
+    //During 5 seconds, makes the player immune to damage, but prevents him from attacking casting abilities or using consumables
+
+    private PlayerMovement movementScript;
+    private PlayerAttack attackScript;
+    private Inventory inventoryScript;
+    private PlayerState healthScript;
+
+    public override void Consume()
+    {
+        base.Consume();
+        movementScript = player.GetComponent<PlayerMovement>();
+        attackScript = player.GetComponent<PlayerAttack>();
+        inventoryScript = player.GetComponent<Inventory>();
+        healthScript = player.GetComponent<PlayerState>();
+        StartCoroutine(LilyEffect(5f));
+    }
+
+    IEnumerator LilyEffect(float time)
+    {
+        //healthScript.isImmune = true;
+        movementScript.canMove = false;
+        //attackScript.canAttack = false;
+        //inventoryScript.canUseConsumable = false;
+        yield return new WaitForSeconds(time);
+        //healthScript.isImmune = false;
+        movementScript.canMove = true;
+        //attackScript.canAttack = true;
+        //inventoryScript.canUseConsumable = true;
+        Destroy(this);
+    }
+
+}
