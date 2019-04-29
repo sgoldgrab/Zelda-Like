f7ff98d60eea2cbaafb1c68f189eadbd3be3000c
simon <s.goldgrab@rubika-edu.com>
2019-04-29 17:11:41 +0200

    +1 consommable
    en note comment créer un unity event dans le script enemy state (la copy de enemyState s'appelle TestSimonEnemyManager)
    le consommable qui tuilise ça c'ets IrisSoakedInMoonlight

---
+++ Zelda-like Project/Assets/Scripts/Simon/WorkOnSandBox/IrisSoakedInMoonlight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IrisSoakedInMoonlight : PickUp
+{
+
+    #region Variables
+    private TestSimonCooldownReduction abilitiesScript;
+    #endregion
+
+    public override void Consume()
+    {
+        base.Consume();
+        abilitiesScript = player.GetComponent<TestSimonCooldownReduction>();
+        StartCoroutine(IrisEffect(10f));
+    }
+
+    private void OnEnable()
+    {
+        TestSiimonEnemyManager.whenEnemyDies += CDRbuff;
+    }
+
+    private void OnDisable()
+    {
+        TestSiimonEnemyManager.whenEnemyDies -= CDRbuff;
+    }
+
+    private void CDRbuff()
+    {
+        abilitiesScript.cooldownReduction += 10;
+    }
+
+    IEnumerator IrisEffect(float time)
+    {
+        abilitiesScript.cooldownReduction +=10;
+        yield return new WaitForSeconds(time);
+        Destroy(this);
+    }
+
+}
