abdd0bbd80ff0261b8a24569ff1ccb44a809a9ee
maelRicou <m.ricou@rubika-edu.com>
2019-05-17 15:05:36 +0100

    Commit Mayel

---
+++ Zelda-like Project/Assets/Scripts/Mael/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    private Sprite sprite;
+
+    private Altar altar;
+
+    [SerializeField] private int index;
+
+    private void Start()
+    {
+        altar = GameObject.Find("Altar" + (index).ToString()).GetComponent<Altar>();
+        //altar = GameObject.Find("Altar").GetComponent<Altar>();
+        image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (altar.hasBeenFinallyActivatedAfterAllTheseYears)
+        {
+            sprite = altar.zeSprite;
+            image.sprite = sprite;
+        }
+
+    }
+
+
+    
+}
